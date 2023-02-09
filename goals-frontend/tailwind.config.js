9b2f1c65b390a7fe4f560392ff1845a1e3b4a3f1
mxoMbatha <mxmbatha02@gmail.com>
2023-02-09 15:06:30 +0200

    commit

--- goals-frontend/tailwind.config.js
+++ goals-frontend/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     './pages/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{html,js,jsx,ts,tsx}'
+    './Features/**/*.{html,js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
