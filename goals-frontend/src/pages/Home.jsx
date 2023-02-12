f37879d3a3a4e5d61221aa4bae3d532de4c64370
mxoMbatha <mxmbatha02@gmail.com>
2023-02-12 09:10:10 +0200

    designed and developed the navigation compenent. Its responsive with a hamburger menu dropdown on smaller screens

--- goals-frontend/src/pages/Home.jsx
+++ goals-frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='container  mx-auto'>
+    <div className='container p-2 mx-auto'>
         <h1>home</h1>
     </div>
   )
--- goals-frontend/tailwind.config.js
+++ goals-frontend/tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
             
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976PX',
+      xl:'1440px',
+      xxl:'1600px',
+    },
+      extend: {
+        colors:{
+          fanta:'hsl(12,88%,59%)',
+          fantaLight:'hsl(12,88%,69%)',
+          fantaSuperLight:'hsl(12,88%,95%)',
+          darkBlue:'hsl(228,39%,23%)',
+          darkGrayishBlue:'hsl(227,12%,61%)',
+          veryDarkBlue:'hsl(223,12%,13%)',
+          veryPaleFanta:'hsl(13,100%,96%)',
+          darkBlueish:'hsl(0,0%,98%)',
+        }
+      },
+    },
+    plugins: [],
 }
