63b0a16b618b630a834b86dc0192fc2b3d68b44b
mxoMbatha <mxmbatha02@gmail.com>
2023-05-15 15:29:32 +0200

    created controls and and laid out a design for the dashboard

---
+++ goals-frontend/src/features/setGoalForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const setGoalForm = () => {
+  return (
+      <div className='form'>
+          <form action="" method="post">
+              <div className="form-group"></div>
+          </form>
+    </div>
+  )
+}
+
+export default setGoalForm
--- goals-frontend/src/pages/Dashboard.jsx
+++ goals-frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useSelector} from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Qoutes from '../features/Qoutes';
+import {FaBars} from 'react-icons/fa';
+import setGoalForm from '../features/setGoalForm'
 const Dashboard = () =>
 {
     const navigate = useNavigate();
@@ -17,11 +19,26 @@ const Dashboard = () =>
      <>
       <div className='container p-2 mx-auto'>
         
-      <h1 className='md:text-4xl text-2xl text-center p-4 font-bold'>Welcome to your Dashboard {user && user.firstName}</h1>
-      <div className="px-6 py-2 text-sm"><Qoutes/></div>
+      <h1 className='md:text-4xl text-2xl text-gray-200 text-left pl-5  font-bold'>Dashboard</h1>
+        
+        <div className="sm:container">
+          <div className="shadow py-2 px-3 gird mt-2">
+            <div className="px-6 py-2 text-sm "><Qoutes /></div>
+          <div className="flex justify-between p-2">
+            <div className='pt-2'><FaBars /></div>
+            <button type="submit" className='px-2 font-bold shadow text-fanta right py-1 hover:bg-gray-300 hover:text-white hover:px-3 transition delay-100 ease-in-out'>set goal</button>
+            </div>
+          </div>  
+        </div>
+        <div className="sm:container rounder p-2">
+          <setGoalForm/>
+
+        </div>
       </div>
+      
+
       </>
   )
 }
 
-export default Dashboard   
+export default Dashboard  
